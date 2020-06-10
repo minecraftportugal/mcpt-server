c7f9fde16ef0bddb14c90ae97a57ef68f302b2bf
Mário C <emailoftheyear@gmail.com>
2020-06-10 10:34:11 +0000

    Add more unconfigured garbo

--- db/docker-entrypoint-initdb.d/init.sql
+++ db/docker-entrypoint-initdb.d/init.sql
@@ -2,6 +2,8 @@ CREATE USER minecraft IDENTIFIED BY 'minecr4ft';
 
 CREATE DATABASE minecraft_cp;
 CREATE DATABASE minecraft_luckperms;
+CREATE DATABASE minecraft_auth;
 
 GRANT ALL PRIVILEGES ON minecraft_cp.* TO minecraft;
 GRANT ALL PRIVILEGES ON minecraft_luckperms.* TO minecraft;
+GRANT ALL PRIVILEGES ON minecraft_auth.* TO minecraft;
